19a233e55883f7e276256b52c986c9eeb0641dd6
Vlad Kresov <vladislavkresov@gmail.com>
2021-12-30 23:22:06 +0300

    vite + three js init

---
+++ main.js
@@ -0,0 +1,15 @@
+import './style.css'
+
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.Camera(75, window.innerWidth / window.innerHeight, 0.1, 100);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector('#background'),
+})
+
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
+
+renderer.render(scene, camera);
